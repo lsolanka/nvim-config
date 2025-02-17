92d29b9a0b6fd5ee326184ab76322cb3fe7092d4
Lukas Solanka <lukas.solanka@sevensense.ch>
2025-02-17 13:51:18 +0000

    set trouble symbols window size to 30%

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/trouble.nvim",
+    cmd = { "Trouble" },
+    opts = {
+      modes = {
+        symbols = { -- Configure symbols mode
+          win = {
+            size = 0.3, -- 30% of the window
+          },
+        },
+      },
+    },
+  },
+}
