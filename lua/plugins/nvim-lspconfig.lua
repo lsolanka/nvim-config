8cf86360ce6f3ac98c35d4a2b95d3e099dd621e5
Lukas Solanka <lukas.solanka@sevensense.ch>
2025-03-28 16:01:10 +0100

    add clangd config

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,5 +2,20 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      clangd = {
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--background-index-priority=low",
+          "-j", "10",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+        },
+      },
+    },
   },
 }
